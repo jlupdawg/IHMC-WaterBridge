802fae6609a2143f5d18fe8a7a877e9bee301f2f
Neploid <jam196@students.uwf.edu>
2018-10-13 21:00:54 -0500

    Two_Sonar
    JAM

--- Arduino/Two_Sonar/Two_Sonar.ino
+++ Arduino/Two_Sonar/Two_Sonar.ino
@@ -1,65 +1,76 @@
+#include <math.h>
 
-const int pwPin1 = 3;
-const int pwPin2 = 5;
-int triggerPin1 = 2; //trigger pin to tell one sensor to fire at a delayed rate to avoid interference
-double sensor1, sensor2, distance1, distance2;
+//const int (Pin-On-Sensor)_(Sensor-Number) = (Pin-On-Arduino)
+const int Pin2_1 = 3;
+const int Pin2_2 = 5;
+const int Pin4_2 = 2; //trigger pin
+const int ThermistorPin = 0;
+double Sensor1, Sensor2, Distance1, Distance2, DistanceAverage;
+double AmbientTemp; //Celcius
+double CalibrationFactor;
+
 
 void setup () {
   Serial.begin(9600);
-  pinMode(pwPin1, INPUT);
-  pinMode(pwPin2, INPUT);
-  pinMode(triggerPin1, OUTPUT);
+  pinMode(Pin2_1, INPUT);
+  pinMode(Pin2_2, INPUT);
+  pinMode(Pin4_2, OUTPUT); //Using pin 2 for triggering asymetric firing of sonar sensor.
 }
 
-double AmbientTemp; //Celcius
-double CalibrationFactor;
-int ThermistorPin = A0;
-int Vo;
-float R1 = 10000;
-float logR2, R2, T, Tc, Tf;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
-
 double CalibrationFactorCalculation(){
+  double RawTemp = analogRead(0); //Reads raw temperature data, stores it in RawTemp
+  
+  AmbientTemp = log(10000.0*((1024.0/RawTemp-1))); //= log(10000.0/(1024.0/RawTemp-1)) // for pull-up configuration
+  AmbientTemp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * AmbientTemp * AmbientTemp ))* AmbientTemp );
+  AmbientTemp = AmbientTemp - 273.15;            // Convert Kelvin to Celcius
     
-  Vo = analogRead(ThermistorPin);
-  R2 = R1 * (1023.0 / (float)Vo - 1.0);
-  logR2 = log(R2);
-  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
-  AmbientTemp = T - 273.15; //celcius
-       
-  CalibrationFactor = (1 / (331 + 0.6 * (AmbientTemp))) * (20000); //Use SI units for calculation
+  CalibrationFactor = (1 / (331 + (0.6 * (AmbientTemp)))) * (20000); //Use SI units for calculation
+  
+  if(AmbientTemp >= 270 || AmbientTemp <= -270){ //Safeguard for failed Thermistor
+      CalibrationFactor = 58.3; //Factory calibration factor
+    }
   return CalibrationFactor;
   }
 
-void read_sensor(){
-  sensor1 = pulseIn(pwPin1, HIGH);
-  distance1 = sensor1 / CalibrationFactorCalculation(); //makes the reported range the distance in centimeters
-  delay(10); //helped make the range readings more stable
-  sensor2 = pulseIn(pwPin2, HIGH);
-  distance2 = sensor2 / CalibrationFactorCalculation();
+void Read_Sensor(){
+  Sensor1 = pulseIn(Pin2_1, HIGH);
+  Distance1 = Sensor1 / CalibrationFactorCalculation(); //makes the reported range the distance in centimeters
+  
+  delay(5); //helped make the range readings more stable
+  
+  Sensor2 = pulseIn(Pin2_2, HIGH);
+  Distance2 = Sensor2 / CalibrationFactorCalculation();
+
+  //DistanceAverage = (Distance1 + Distance2) / 2.00;
 }
 
-void start_sensor(){
-  digitalWrite(triggerPin1,HIGH);
-  delay(5);   //delay between triggering the sensor that has UNO pin 2 to 4 on the sensor
-  digitalWrite(triggerPin1,LOW);
+void Start_Sensor(){
+  digitalWrite(Pin4_2,HIGH);
+  delay(2);   //Trigger delay in ms
+  digitalWrite(Pin4_2,LOW);
 }
 
 //This section of code is if you want to print the range readings to your computer too remove this from the code put /* before the code section and */ after the code
-void printall(){         
+void Print_All(){         
   Serial.print("S1: ");
   Serial.print(" ");
-  Serial.print(distance1);
+  Serial.print(Distance1);
   Serial.print(" ");
   Serial.print("S2: ");
   Serial.print(" ");
-  Serial.print(distance2); 
+  Serial.print(Distance2); 
+  /*
+  Serial.print(" ");
+  Serial.print("Avg: ");
+  Serial.print(" ");
+  Serial.print(DistanceAverage); 
+  */
   Serial.println();
 }
 
 void loop () {
-  start_sensor();
-  read_sensor();
-  printall();
-  delay(20);  //delay before the void loop starts the section again.  Make this match the refresh rate times how ever many sensors are in the chain.  I typically add 10 to help with any secondary reflections
+  Start_Sensor();
+  Read_Sensor();
+  Print_All();
+  delay(15);  //delay before the void loop starts the section again.  Make this match the refresh rate times how ever many sensors are in the chain.  I typically add 10 to help with any secondary reflections
 }
