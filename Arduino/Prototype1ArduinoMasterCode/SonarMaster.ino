9b11c783d4f9c2c7aab6b1a071215f491805d836
Neploid <jam196@students.uwf.edu>
2018-11-20 18:34:33 -0600

    Partial Sonar/Object Avoidance Code
    This is a partially written code for the object avoidance using the ultra-snonic sonar sensor system.

--- Arduino/Prototype1ArduinoMasterCode/SonarMaster.ino
+++ Arduino/Prototype1ArduinoMasterCode/SonarMaster.ino
@@ -1,73 +1,56 @@
-int CollisionWarning, Left = 0, Right = 0, Centered = 0; //LEft and Right are relative to the front sonar sensor/front of boat
+int Direction = 3; //Left and Right are relative to the front sonar sensor/front of boat
 
-double Override(int f1, int b1){ //
-  int CollisionWarning;
-  if(f1 <= 20.00){ //20centimeters
-      CollisionWarning = 1; //if CollisionWarning equals 1 then there is an object within 20cm
-    }
-  else{
-      CollisionWarning = 0; //No object within 20cm
-    }
-  
-  //return CollisionWarning;
-  }
 
-void Object_Location(){
+void Object_Location() {
 
-  if(leftMotor == 0 && rightMotor ==0){
+  if (leftMotorValue == 1500 && rightMotorValue == 1500) {
     f1 = SonarSensor_Front_Back(Pin4_2, Pin2_1);
     b1 = SonarSensor_Front_Back(Pin4_2, Pin2_2);
   }
-  
-  if(leftMotor > 0 && rightMotor > 0){
-    f1 = SonarSensor_Front_Back(Pin4_2, Pin2_1);
-  }
-  
-  if(leftMotor < 0 && rightMotor < 0){
-    b1 = SonarSensor_Front_Back(Pin4_2, Pin2_2);
-  }
-  
-  if(f1 <= 20){
-    s1 = SonarSensor_Corner(trigPin_1, echoPin_1);
-    s2 = SonarSensor_Corner(trigPin_2, echoPin_2);
-    
-    if(s1 < s2){
-      Left = 1;
-    }
-    
-    else if(s2 < s1){
-      Right = 1;
-    }
-    
-    else{
-      Centered = 1;
-    }
-    
-  }
-  else{
-    s1 = s2 = 0;
-    }
 
-  if(b1 <= 20){
-    
-    s3 = SonarSensor_Corner(trigPin_3, echoPin_3);
-    s4 = SonarSensor_Corner(trigPin_4, echoPin_4);
-    
-    if(s3 < s4){
-      Right = 1;
+  if (leftMotorValue > 1500 && rightMotorValue > 1500) {
+    f1 = SonarSensor_Front_Back(Pin4_2, Pin2_1);
+
+    if (f1 <= 20) {
+      s1 = SonarSensor_Corner(trigPin_1, echoPin_1);
+      s2 = SonarSensor_Corner(trigPin_2, echoPin_2);
+
+      if (s1 < s2) {
+        Direction = 1; // 1 equals Left
+      }
+
+      else if (s2 < s1) {
+        Direction = 0; //0 equals Right
+      }
+      else {
+        Direction = 2; // 2 equals centered
+      }
     }
-    
-    else if(s4 < s3){
-      Left = 1;
+    else {
+      s1 = s2 = 0;
     }
-    
-    else{
-      Centered = 1;
-    }
-    
   }
-  else{
-    s3 = s4 = 0;
+  if (leftMotorValue < 1500 && rightMotorValue < 1500) {
+    b1 = SonarSensor_Front_Back(Pin4_2, Pin2_2);
+
+    if (b1 <= 20) {
+      s3 = SonarSensor_Corner(trigPin_3, echoPin_3);
+      s4 = SonarSensor_Corner(trigPin_4, echoPin_4);
+
+      if (s3 < s4) {
+        Direction = 0;
+      }
+
+      else if (s4 < s3) {
+        Direction = 1;
+      }
+
+      else {
+        Direction = 2;
+      }
     }
-  
+    else {
+      s3 = s4 = 0;
+    }
+  }
 }
