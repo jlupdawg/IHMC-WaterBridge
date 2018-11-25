ac2b27d74a63a491ff4249c48d988d907e848ad9
brendonortolano <44093569+brendonortolano@users.noreply.github.com>
2018-11-25 15:46:38 -0600

    Created framework for docking code

---
+++ DockingCode/DockingCode.ino
@@ -0,0 +1,45 @@
+float sonarDist1; //holds the distances sensed by the dock sonars
+float sonarDist2;
+
+float dockingValue = 20; //maximum sonar distance to consider boat "docked"
+
+float distBetweenSensors; //distance between the mounted sensors
+
+float theta; //angle between the sonar pulses
+float alpha; //angle between triangle created by sonar pulses and the vector negative of the direction the dock is facing
+float desiredHeading;
+float currentHeading;
+float dockDirection; //vector in the direction the dock is facing
+
+boolean verifiedBoat; //stores whether the boat has confirmed that it is the object returning sonar pulses
+boolean dockingMode;
+boolean boatDocked; //stores whether or not the dock has completed docking
+
+void loop() {
+
+  while (dockingMode){
+
+    if(verifiedBoat == false){
+      pulseSonar();
+      }
+
+    //readSensor1(); //Radio
+    //readSensor2(); //Radio
+    
+    lawOfCosines(sonarDist1, sonarDist2, distBetweenSensors);
+    findAlpha();
+    
+    calculateDesiredHeading();
+    //getDesiredHeading;
+    
+    changeHeading();
+
+    if(boatDocked == false){
+      checkDist();
+      }
+    
+  }
+
+  
+
+}
---
+++ DockingCode/Steering.ino
@@ -0,0 +1,24 @@
+void changeHeading(){
+  
+  if(currentHeading<desiredHeading){
+    
+    
+    
+    }
+
+  else if(currentHeading>desiredHeading){
+    
+    
+    
+    }
+  
+  }
+
+void checkDist(){
+  
+  if(sonarDist1 < dockingValue || sonarDist2 < dockingValue){
+    //stopMotors();
+    boatDocked = true;
+    }
+  
+  }
