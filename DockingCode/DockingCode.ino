c9e89cb2ae2f051ec03b2a1df277d829fd4ee3fc
spodermanm3 <44093972+spodermanm3@users.noreply.github.com>
2018-12-02 22:11:30 -0600

    nothing

--- DockingCode/DockingCode.ino
+++ DockingCode/DockingCode.ino
@@ -10,7 +10,7 @@ float alpha; //angle between triangle created by sonar pulses and the vector neg
 float desiredHeading;
 float currentHeading;
 float dockDirection; //vector in the direction the dock is facing
-
+float headingError;
 boolean verifiedBoat; //stores whether the boat has confirmed that it is the object returning sonar pulses
 boolean dockingMode;
 boolean boatDocked; //stores whether or not the dock has completed docking
