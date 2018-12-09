6aa239c61f3fbd7055291f85ebc4b4ec6f57daf2
jlupdawg <jcl54@students.uwf.edu>
2018-12-09 16:47:41 -0600

    slight update

--- Arduino/Prototype1ArduinoMasterCode/Prototype1ArduinoMasterCode.ino
+++ Arduino/Prototype1ArduinoMasterCode/Prototype1ArduinoMasterCode.ino
@@ -232,34 +232,34 @@ int dockingRegion;
 
 void loop() {
   dockingMode = false;
-    //writeLCD_Motors();//NEW
+  //writeLCD_Motors();//NEW
   //Serial.println("loop");
-  if(dockingRegion != 4 && dockingRegion != 5){
+  if (dockingRegion != 4 && dockingRegion != 5) {
     //dockingMode = true;
-    }
+  }
 
   if ((controllerMode == false) && (dockingMode == false) && (objectIndicated == 0)) { //-------------must reset the master board after putting the boat in controllerMode. This is intentional
-incomingRadio();            // ------------------------------------------------------------------reads incoming radio and sends it to the motors. This may need to be changed to "Incoming Radio" for future use
+    incomingRadio();            // ------------------------------------------------------------------reads incoming radio and sends it to the motors. This may need to be changed to "Incoming Radio" for future use
     readSerial();               // ------------------------------------------------------------------check incoming serial communication
 
-    
-    if(newSerialData == true)
+
+    if (newSerialData == true)
     {
       strcpy(tempSerialChars, receivedSerialChars);
       printInByte();            // ------------------------------------------------------------------printInbyte and decide on whether or not the motors should be updated and prints the value
-    }             
-    //Object_Location();
+    }
+    Object_Location();
 
     //Object_Location();
 
     //Serial.println("PRINT 7");
-    
+
     if (updateMotors) {
       setMotors_Serial();       // ------------------------------------------------------------------set the motors with pwm pin values
       writeLCD_Motors();
       //writeLCD(inByte[1][1], inByte[2][1]);
     }
-    
+
 
     //loggingData("Normal");
     newSerialData = false;
